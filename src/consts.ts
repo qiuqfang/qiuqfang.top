bf0d10399ed360742869fac4c9f43f4baf48cc28
Quincy Qiu <qiuqfang@outlook.com>
2023-03-21 15:40:27 +0800

    chore: update description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Quincy Qiu';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Quincy Qiu";
+export const SITE_DESCRIPTION =
+  "This is my personal blog site, my name is Quincy Qiu, I am a front-end developer, I can React, Vue, Astro, Electron and other front-end development technologies, I like to learn new technologies.";
