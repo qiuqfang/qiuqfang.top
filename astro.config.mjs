6480f71d79c7041943ae3644d9428124948e2433
qiuqfang <qiuqfang@outlook.com>
2023-06-18 20:02:15 +0800

    chore: 修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.qiuqfang.top",
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { path: "./tailwind.config.cjs", applyBaseStyles: false },
-    }),
-  ],
+  integrations: [sitemap(), tailwind()],
 });
